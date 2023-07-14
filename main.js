8bbb333c9be1b705ae607a71a6bbd8093a4528a5
hyeongyun <guddbs5545@naver.com>
2023-07-14 12:26:46 +0900

    deatils 추가

--- main.js
+++ main.js
@@ -3,3 +3,20 @@ const loginorout = document.getElementById("loginText")
 if(window.localStorage.getItem("token")){
     loginorout.appendChild(`<a href="./login.html">Logout</a>`)
 }
+
+fetch('http://3.38.245.204:8081/products')
+  .then(response => response.json())
+  .then(data => {
+    // 데이터를 기반으로 HTML 요소 생성
+    const tags = data.map(item => {
+      const tag = document.createElement('div');
+      tag.textContent = item.name;
+      return tag;
+    });
+    console.log(tags)
+  })
+
+
+  .catch(error => {
+    console.error('API 요청 중 오류 발생:', error);
+  });
