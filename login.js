38976af30cc20515af554ac17d6c856e8af972f8
HYEONGYUN\guddb <guddbs5545@naver.com>
2023-07-15 13:30:37 +0900

    Login Ui 다시 변경

--- login.js
+++ login.js
@@ -1,64 +1,76 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signInBtn');
-const container = document.getElementById('container');
-const username = document.getElementById("username");
-const password = document.getElementById("password");
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signInBtn');
+// const container = document.getElementById('container');
+// const username = document.getElementById("username");
+// const password = document.getElementById("password");
 
-// signUpButton.addEventListener('click', () => {
-// 	container.classList.add("right-panel-active");
-// });
-
-
+// // signUpButton.addEventListener('click', () => {
+// // 	container.classList.add("right-panel-active");
+// // });
 
 
-function onSignIn() {
 
-     // singIn post Url
-    //  const geturl = "http://a876cfdd49d8a48c5b314d85b5c6f2ff-2070479410.ap-northeast-2.elb.amazonaws.com:5000/tweets";
 
-    //  // JSON 데이터를 서버로 전송하는 fetch 요청
-    //  fetch(geturl, {
-    //    method: "GET",
-    //    headers: {
-    //      "Content-Type": "application/json"
-    //    },
-    // //    body: JSON.stringify(user)
-    //  })
-    //  .then(response => response.text())
-    //  .then(result => console.log(result))
-    //    .catch(function(error) {
-    //      console.error("Error sending user data:", error);
-    //    });
+// function onSignIn() {
 
-    console.log(typeof(username.value))
-    console.log(password.value)
+//      // singIn post Url
+//     //  const geturl = "http://a876cfdd49d8a48c5b314d85b5c6f2ff-2070479410.ap-northeast-2.elb.amazonaws.com:5000/tweets";
 
-    let user = {
-        "username": username.value,
-        "password": password.value
-    }
+//     //  // JSON 데이터를 서버로 전송하는 fetch 요청
+//     //  fetch(geturl, {
+//     //    method: "GET",
+//     //    headers: {
+//     //      "Content-Type": "application/json"
+//     //    },
+//     // //    body: JSON.stringify(user)
+//     //  })
+//     //  .then(response => response.text())
+//     //  .then(result => console.log(result))
+//     //    .catch(function(error) {
+//     //      console.error("Error sending user data:", error);
+//     //    });
+
+//     console.log(typeof(username.value))
+//     console.log(password.value)
+
+//     let user = {
+//         "username": username.value,
+//         "password": password.value
+//     }
     
-    // singIn post Url
-    const url = "http://a876cfdd49d8a48c5b314d85b5c6f2ff-2070479410.ap-northeast-2.elb.amazonaws.com:5000/auth/login";
+//     // singIn post Url
+//     const url = "http://a876cfdd49d8a48c5b314d85b5c6f2ff-2070479410.ap-northeast-2.elb.amazonaws.com:5000/auth/login";
 
-    // JSON 데이터를 서버로 전송하는 fetch 요청
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
-    })
-    .then(response => response.text())
-    .then(result => {
-        console.log(result)
-        window.localStorage.setItem("token", result.slice(10, 166))
-        console.log(window.localStorage.getItem("token"))
-    })
-      .catch(function(error) {
-        console.error("Error sending user data:", error);
-      });
+//     // JSON 데이터를 서버로 전송하는 fetch 요청
+//     fetch(url, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify(user)
+//     })
+//     .then(response => response.text())
+//     .then(result => {
+//         console.log(result)
+//         window.localStorage.setItem("token", result.slice(10, 166))
+//         console.log(window.localStorage.getItem("token"))
+//     })
+//       .catch(function(error) {
+//         console.error("Error sending user data:", error);
+//       });
 
-    }
+//     }
 
-    signInButton.addEventListener('click', onSignIn);
+//     signInButton.addEventListener('click', onSignIn);
+
+const sign_in_btn = document.querySelector("#sign-in-btn");
+const sign_up_btn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+sign_up_btn.addEventListener("click", () => {
+  container.classList.add("sign-up-mode");
+});
+
+sign_in_btn.addEventListener("click", () => {
+  container.classList.remove("sign-up-mode");
+});
